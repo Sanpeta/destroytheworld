25b8e84c0e64592fe095d9e28822c1569ef174f3
Sidnei de Souza Junior <sidneisonic@hotmail.com>
2019-08-26 00:37:54 -0300

    Adicionando os arquivos no destroytheworld

---
+++ src/App.vue
@@ -0,0 +1,92 @@
+<template>
+	<div id="app" class="container">
+		<h1>HTTP com Axios</h1>
+		<b-card>
+			<b-form-group label="Nome:">
+				<b-form-input 
+				type="text" 
+				size="lg" 
+				v-model="usuario.nome" 
+				placeholder="Informe o Nome">
+				</b-form-input>
+			</b-form-group>
+			<b-form-group label="E-mail:">
+				<b-form-input 
+				type="email" 
+				size="lg" 
+				v-model="usuario.email" 
+				placeholder="Informe o E-mail">
+				</b-form-input>
+			</b-form-group>
+			<b-button @click="salvar" size="lg">Salvar</b-button>
+			<b-button @click="obterUsuarios"
+				size="lg"
+				variant="success"
+				class="ml-2">
+				Obter Usuarios
+				</b-button>
+		</b-card>		
+		<hr>
+		<b-list-group>
+			<b-list-group-item v-for="(usuario, id) in usuarios" :key="id">
+				<strong>Nome: </strong>{{usuario.nome}}<br>
+				<strong>E-mail: </strong>{{usuario.email}}<br>
+				<strong>ID: </strong>{{id}}<br>
+			</b-list-group-item>
+		</b-list-group>
+	</div>
+</template>
+
+<script>
+export default {
+
+	data() {
+		return {
+			usuarios: [
+
+			],
+			usuario: {
+				nome: '',
+				email: ''
+			}
+		}
+	},
+	methods: {
+		salvar(){
+			this.$http.post('usuarios.json', this.usuario)
+				.then(() => {
+					this.usuario.nome = ''
+					this.usuario.email = ''
+				})
+		},
+		obterUsuarios()	{
+			this.$http.get('usuarios.json')
+				.then(res => {
+					this.usuarios = res.data					
+				})
+		}
+	},
+	// created() {
+	// 	this.$http.post('usuarios.json', {
+	// 		nome: 'Sidnei',
+	// 		email: 'sidnei@gmail.com'
+	// 	})
+	// }
+
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	color: #2c3e50;
+	font-size: 1.5rem;
+}
+
+#app h1 {
+	text-align: center;
+	margin: 50px;
+}
+</style>
